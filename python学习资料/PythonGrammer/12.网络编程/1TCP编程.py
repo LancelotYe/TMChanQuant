2c19640236191ea6529fc063cc007caac803411d
Tom <973152312@qq.com>
2019-12-03 00:02:16 +0800

    邮件 ——Tom

--- python学习资料/PythonGrammer/12.网络编程/1TCP编程.py
+++ python学习资料/PythonGrammer/12.网络编程/1TCP编程.py
@@ -59,7 +59,7 @@ import socket
 # 创建一个socket:
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # 建立连接:
-s.connect(('www.sina.com.cn', 80))/
+# s.connect(('www.sina.com.cn', 80))
 
 # 创建Socket时，AF_INET指定使用IPv4协议，如果要用更先进的IPv6，就指定为AF_INET6。SOCK_STREAM指定使用面向流的TCP协议，这样，一个Socket对象就创建成功，但是还没有建立连接。
 #
